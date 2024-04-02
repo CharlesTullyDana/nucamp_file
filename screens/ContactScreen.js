8aac361a9355165038b356a680270337f991d28b
C Tully Dana <Tullyprofessionalcodingsyntax@gmail.com>
2024-04-02 16:21:27 -0700

    Login and register tabs working

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,59 +1,49 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Card, Text } from 'react-native-elements';
+import { ScrollView, Text } from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
+
 const ContactScreen = () => {
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        });
+    };
+
     return (
         <ScrollView>
-              <Animatable.View
-            animation='fadeInDownBig'
-            duration={2000}
-            delay={1000}
-            >  
-            <Card wrapperStyle={{ margin: 20 }}>
-                <Card.Title>Contact Information</Card.Title>
-                <Card.Divider />
-
-                <Text style={styles.text}>
-                    1 Nucamp Way{'\n'}
-                    Seattle, WA 98001{'\n'}
-
-                    U.S.A. </Text>
-
-                <Text style={styles.text}>
-                    Phone: 1-206-555-1234
-                </Text>
-
-                <Text style={{marginBottom: 10}}>
-                    Email: campsites@nucamp.co
-                </Text>
-            </Card>
-            </Animatable.View> 
-        </ScrollView>   
+            <Animatable.View
+                animation='fadeInDown'
+                duration={2000}
+                delay={1000}
+            >
+                <Card wrapperStyle={{ margin: 20 }}>
+                    <Card.Title>Contact Information</Card.Title>
+                    <Card.Divider />
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                    <Button
+                        title='Send Email'
+                        buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+                        icon={
+                            <Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{ marginRight: 10 }}
+                            />
+                        }
+                        onPress={() => sendMail()}
+                    />
+                </Card>
+            </Animatable.View>
+        </ScrollView>
     );
 };
 
-const styles = StyleSheet.create({
-    text: {
-        marginBottom: 10
-    }
-});
-
-const ContactNavigator = () => {
-    const Stack = createStackNavigator();
-    const screenOptions = {
-        headerTintColor: '#fff',
-        headerStyle: { backgroundColor: '#5637DD' }
-    };
-    return (
-        <Stack.Navigator screenOptions={screenOptions}>
-            <Stack.Screen
-                name='Contact'
-                component={ContactScreen}
-            />
-        </Stack.Navigator>
-    );
-};
-
-export default ContactNavigator;
+export default ContactScreen;
